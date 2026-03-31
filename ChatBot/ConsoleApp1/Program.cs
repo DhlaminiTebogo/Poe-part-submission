838ae184b9e85f03f5e30f6aa95d1a064bd1c2c2
DhlaminiTebogo <ST10487180@imconnect.edu.za>
2026-03-31 21:32:42 +0200

    Create Program.cs
    Create a program file the using other classes through program.cs

---
+++ ChatBot/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1;
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var bot = new Bot();
+
+        bot.Start();
+    }
+}
