f1110e470d9d044e4bbf802d6f6a42f1f8bf8f1a
DhlaminiTebogo <ST10487180@imconnect.edu.za>
2026-03-31 21:27:05 +0200

    Class: Audio greeting
    Create a class that handles Audio greeting and wav file for audio greeting is include

---
+++ ChatBot/ConsoleApp1/AudioGreeting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace ConsoleApp1
+{
+    public class AudioGreeting
+    {
+        public static void PlayGreeting(string filePath = "AudioGreeting.wav")
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    using var player = new SoundPlayer(filePath);
+                    player.PlaySync();
+                }
+                else
+                {
+                    ConsoleUIMethods.PrintError("Greeting file not found");
+                }
+            }
+            catch
+            {
+                ConsoleUIMethods.PrintError("Audio playback error");
+            }
+        }
+    }
+}
